d1f792195926bb70f7e05eed6544cb685be10288
A-007481D <labidabdelmalek@gmail.com>
2024-11-28 11:34:34 +0100

    modal open close

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function openAddPM() {
+    const addModal = document.getElementById("AddPlayer-modal");
+    addModal.style.display ="flex"; 
+ }
+ 
+ function closeAddModal() {
+  const modal = document.getElementById("AddPlayer-modal");
+  modal.style.display = "none";
+}
