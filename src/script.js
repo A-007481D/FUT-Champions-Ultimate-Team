10dd7a585f5b45df009ecee7625ded1df6f94a88
A-007481D <labidabdelmalek@gmail.com>
2024-12-02 23:40:33 +0100

    edit player...

--- src/script.js
+++ src/script.js
@@ -451,9 +451,37 @@ playerPosition.addEventListener("change", function () {
   }
 });
 
+let selectedP;
+
 function openEditPM() {
-  const addModal = document.getElementById("editPlayer-modal");
-  addModal.style.display = "flex";
+  const editModal = document.getElementById("editPlayer-modal");
+  editModal.style.display = "flex";
+
+  let name = selectedP.querySelector('.player-NAME');
+
+  let player = playerArray.filter(player => player.name == name.textContent)[0];
+  editModal.querySelector('#playerName').value = player.name;
+  editModal.querySelector('#playerNationality').value = player.nationality;
+  editModal.querySelector('#ClubLogo').value = player.club;
+
+  editModal.querySelector('#addEdited-player').onclick = function () {
+    for (let i = 0; i < playerArray.length; i++) {
+      if (playerArray[i].name == player.name) {
+        playerArray[i].name = editModal.querySelector('#playerName').value;
+        playerArray[i].nationality = `https://flagsapi.com/${editModal.querySelector('#playerNationality').value.toUpperCase()}/flat/64.png`
+        playerArray[i].club = `https://raw.githubusercontent.com/A-007481D/FB-Clubs-API/refs/heads/main/teams/${editModal.querySelector('#ClubLogo').value.toUpperCase()}.png`
+
+
+        name.textContent = playerArray[i].name;
+        selectedP.querySelector('.player-flag').src = playerArray[i].nationality;
+        editModal.querySelector('#ClubLogo').value = player.club;
+        selectedP.querySelector('.player-club').src = playerArray[i].club;
+      }
+    }
+    closeEditModal();
+    // https://flagsapi.com/${country}/flat/64.png
+    // https://raw.githubusercontent.com/A-007481D/FB-Clubs-API/refs/heads/main/teams/${club}.png
+  }
 }
 
 function closeEditModal() {
@@ -686,7 +714,7 @@ function renderPlayerList(playerArray) {
           </div>
         </div>
         <div class="flex justify-center space-x-2">
-        <button onclick="openEditPM()" class="edit-btn card-btns bg-green-500 hover:bg-green-700 group-hover:opacity-100 opacity-0 text-white font-bold py-1.5 px-3 rounded">
+        <button class="edit-btn card-btns bg-green-500 hover:bg-green-700 group-hover:opacity-100 opacity-0 text-white font-bold py-1.5 px-3 rounded">
 <svg class="w-4 fill-white" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/></svg></button>
 
 <button class="delete-btn group-hover:opacity-100 opacity-0 card-btns-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1.5 px-3 rounded">
@@ -702,7 +730,8 @@ function renderPlayerList(playerArray) {
 
       playerCard.querySelector('.edit-btn').addEventListener('click', function (e) {
         e.stopPropagation();
-        editPlayer(this.parentElement.parentElement)
+        selectedP = this.parentElement.parentElement;
+        openEditPM();
       })
 
     playerListDiv.appendChild(playerCard);
@@ -726,13 +755,11 @@ function deletePlayer(card) {
 
 
 
-function editPlayer(card) {
-
-  const testcard = element.closest("div[data-id");
-  const cardId = parseInt(testcard.getAttribute('data-id'));
-  const index = playerArray.findIndex(player => player.id === cardId)
-   playerName.value = playerArray[index].name;
-   playerRating.value = playerArray[index].rating;
-   playerClub.value = playerArray[index].club;
-  console.log()
-}
+// function editPlayer(card) {
+//   const testcard = element.closest("div[data-id");
+//   const cardId = parseInt(testcard.getAttribute('data-id'));
+//   const index = playerArray.findIndex(player => player.id === cardId)
+//    playerName.value = playerArray[index].name;
+//    playerRating.value = playerArray[index].rating;
+//    playerClub.value = playerArray[index].club;
+// }
